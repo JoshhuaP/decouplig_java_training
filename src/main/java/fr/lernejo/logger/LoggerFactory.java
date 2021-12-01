f24fc1a93c5137a0a3cd8714d2127c82a08f983f
JoshhuaP <Joshua.pinneau@gmail.com>
2021-12-01 16:18:11 +0100

    Part 5

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,6 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new ConsoleLogger();
+        return new FileLogger("C:\\COURS\\4A\\TPInterfaces\\decoupling_java_training\\test.txt");
     }
 }
